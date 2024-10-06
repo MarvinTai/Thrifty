5fcf84c46f06c914bf0829605c12421db71c1397
Marvin Tai <mt458@duke.edu>
2024-10-06 02:32:19 -0400

    another commit

--- client/src/components/Nav/Nav-Links/NavLinks.js
+++ client/src/components/Nav/Nav-Links/NavLinks.js
@@ -7,10 +7,10 @@ const NavLinks = () => {
                 <div className="bottom__container">
                     <ul className="nav">
                         <li className='nav-link'><Link to="/">Home</Link></li> 
-                        <li className='nav-link'><Link to="/category/men">Jackets</Link></li> 
-                        <li className='nav-link'><Link to="/category/women">Dresses</Link></li> 
-                        <li className='nav-link'><Link to="/category/kids">Pants</Link></li>
                         <li className='nav-link'><Link to="/shop">Shop</Link> </li>
+                        <li className='nav-link'><Link to="/category/men">Nearby Menswear</Link></li> 
+                        <li className='nav-link'><Link to="/category/women">Nearby Womenswear</Link></li> 
+                        <li className='nav-link'><Link to="/category/kids">Nearby Kidswear</Link></li>
                     </ul>
                 </div>
             </nav>
